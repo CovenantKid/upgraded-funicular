eaf02996974ee14dbc1e522a199d1dd2fb78742d
Brad <Bradrockk@gmail.com>
2023-10-18 04:28:14 -0400

    start button now begins the test and destroys itself

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,14 @@
-    document.getElementById("buttonStart").addEventListener("click", log)
-
-
-function log(){
- console.log("I was pressed.")
-}
+   
+//    listens for click and starts quiz when button is pressed.
+   document.getElementById("buttonStart").addEventListener("click", startQuiz);
+//    Defines the new button that is made when the quiz is started.
+    const GenerateButton = document.createElement("button")
+    GenerateButton.textContent = ("Submit answer")
+    GenerateButton.id = ("buttonStart")
+    GenerateButton.addEventListener("click", submitAnswer)
+// called when the user presses start on the quiz
+ function startQuiz(){
+    $('#QuestionText').text("how goes everything?")
+    document.getElementById("buttonStart").remove()
+    document.body.appendChild(GenerateButton)
+ }
