cb06a8ab7b70114c8db624230c973cd6b2c08c18
Brad <Bradrockk@gmail.com>
2023-10-19 04:37:02 -0400

    finished collection of name and score

--- assets/script.js
+++ assets/script.js
@@ -2,43 +2,49 @@
    score = 0;
 
 
+
 // Defines the input that is made when you go to save your score.
    const NameField = document.createElement("input");
-   const SubmitScoreButton = document.createElement("button")
-   const FinalScore = document.createElement("p")
+   NameField.classList.add('QuestionArea')
 
+   const SubmitScoreButton = document.createElement("button")
+   SubmitScoreButton.id = ('ButtonStart')
+   SubmitScoreButton.textContent = ('Submit score')
+   SubmitScoreButton.addEventListener("click", saveScore)
+
+   const ScoreTag = document.createElement("p")
+   ScoreTag.id = ("leaderboard")
 
 
 //    listens for click and starts quiz when button is pressed.
    document.getElementById("buttonStart").addEventListener("click", startQuiz);
-//    Defines the new button that is made when the quiz is started.
-    const GenerateButton = document.createElement("button");
-    GenerateButton.textContent = ("Submit answer");
-    GenerateButton.id = ("buttonSubmit");
-    GenerateButton.classList.add("questionFooter")
-    GenerateButton.addEventListener("click", submitAnswer);
+    // Defines the new button that is made when the quiz is started.
+     const GenerateButton = document.createElement("button");
+     GenerateButton.textContent = ("Submit answer");
+     GenerateButton.classList.add("questionFooter")
+     GenerateButton.id = ("buttonStart");
+     GenerateButton.addEventListener("click", submitAnswer);
 // called when the user presses start on the quiz
  function startQuiz(){
     document.getElementById("buttonStart").remove();
     document.body.appendChild(GenerateButton);
     $('#QuestionText').text("how goes everything?");
-     Quiztime = setInterval(clearQuiz,1000,time= 5,);
+     Quiztime = setInterval(clearQuiz,1000,time= 120,);
 
  }
 
 function clearQuiz(){
-    if (time == 0) {
+    if (time <= 0) {
         $('#QuestionText').text("Time is up!");
          document.getElementById('option1').remove()
          document.getElementById('option2').remove()
          document.getElementById('option3').remove()
          document.getElementById('choices').remove()
-         document.getElementById('buttonSubmit').remove()
+         document.getElementById('buttonStart').remove()
          clearInterval(Quiztime)
         document.body.appendChild(NameField)
         document.body.appendChild(SubmitScoreButton)
-        document.body.appendChild(FinalScore)
-        $('FinalScore').text("Your final score is:",score)
+        
     }
     else{
         time--;
@@ -55,10 +61,19 @@ function submitAnswer(){
     return score
 }
     else{
-     console.log("Incorrect!")   
+     console.log("Incorrect!")
+     time = time - 5   
     }
 }  
 
 function saveScore(){
+Name = NameField.value
+localStorage.setItem("Score",score)
+localStorage.setItem("Name",Name)
+document.body.appendChild(ScoreTag)
+ScoreTag.textContent += score
+ScoreTag.textContent += " "
+ScoreTag.textContent += Name
+ScoreTag.textContent += "      "
 
 }
