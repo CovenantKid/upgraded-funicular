f23dafe56868657a591c5fb491d5d0c8ead6e0b0
Brad <Bradrockk@gmail.com>
2023-10-20 06:19:37 -0400

    added a few questions and quiz ends if you reach the end of them.

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,7 @@
 
 
 
+
 // Defines the input that is made when you go to save your score.
    const NameField = document.createElement("input");
    NameField.classList.add('QuestionArea');
@@ -29,7 +30,7 @@
  function startQuiz(){
     document.getElementById("buttonStart").remove();
     document.body.appendChild(GenerateButton);
-    $('#QuestionText').text(GetQuestion);
+    GetQuestion();
      Quiztime = setInterval(clearQuiz,1000,time= 120,);
 
  }
@@ -53,9 +54,8 @@ function clearQuiz(){
     }
 }
 function submitAnswer(){
-    answer = 1;
     var option = document.querySelector('input[name="choice"]:checked').value;
-    if (option == answer){
+    if (option == CurrentAnswer){
     score = score + 5;
     console.log(score);
     console.log("Correct!");
@@ -81,12 +81,32 @@ ScoreTag.textContent += Name
 ScoreTag.textContent += "      "
 
 }
-
+// storing the text of the question and its answer
 var QuestionTexts=["what tag is used to create a link between HTML and javascript? 1.<link> 2.<script> 3.<div>",
- "Question 2", "question 3"]
+ "Which symbol is used to make a class in CSS? 1. A period 2.A hashtag 3.A dollar sign",
+"read the following line: var numbers = [1,2,3] if you need to select the 2 in this variable, which of the following would work? 1.console.log(numbers) 2.console.log(numbers[1]) 3.console.log(numbers[2])",
+"If you need to get a variable out of a function to the wider code, which of these tags would work? assume the word variable to be the variable in question. 1.escape variable 2.just write the variable's name. 3.return variable",
+"When creating a responsive design page which of the following allows you to create breakpoints in the code for smaller devices? 1. A class 2.An ID 3.A media Querry.",
+"What is Jquery? 1. A function that lets you find the type of data something is. 2. A library 3. A alternative to javascript",
+"If you need to add a new item to the document, which of these will allow you to?  1.appendchild() 2.setitem() 3.neither",
+"what is a constant? 1. Another word for a variable 2.A variable that can't be changed 3.A way to change a variable",
+"What is a p tag in HTML? 1.a way to write javascript without a script 2. A way to write a paragraph 3. none of the above"]
 
+var answer=[1,1,2,3,3,2,1,2,2]
+
+// Selects the next active question from the above variables aswell as the answer for the question
 function GetQuestion(){
-activeQuestion = QuestionTexts[0]
-questionsPassed = questionsPassed + 1
+    if (questionsPassed <= 8){
+activeQuestion = QuestionTexts[questionsPassed];
+$('#QuestionText').text(activeQuestion);
+CurrentAnswer = answer[questionsPassed];
+questionsPassed = questionsPassed + 1;
+return CurrentAnswer;
+}
+ else {
+    console.log("end")
+    clearQuiz(time = 0)
+    $('#QuestionText').text("End of questions! Thanks for your participation!");
+ }
 
 }
